81fbe8ed8f86e3c4f10310e1ce872c3efd9e47ae
yuhyerin <hyerin77@naver.com>
2024-03-01 17:33:10 +0900

    add test case 3

--- src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
+++ src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
@@ -1,8 +1,10 @@
 package productimporter.suppliers.wayneenterprises;
 
+import productimporter.Pricing;
 import productimporter.Product;
 import productimporter.ProductImporter;
 
+import java.math.BigDecimal;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -17,7 +19,9 @@ public final class WayneEnterprisesProductImporter implements ProductImporter {
     @Override
     public Iterable<Product> fetchProducts() {
         return StreamSupport.stream(dataSource.fetchProducts().spliterator(), false)
-                .map(p -> new Product("WAYNE", null, null, null))
+                .map(p -> new Product("WAYNE", p.getId(), p.getTitle(),
+                        new Pricing(new BigDecimal(p.getListPrice()), new BigDecimal(p.getListPrice() - p.getSellingPrice())))
+                )
                 .collect(Collectors.toList());
     }
 
--- src/test/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter_specs.java
+++ src/test/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter_specs.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import productimporter.DomainArgumentsSource;
 import productimporter.Product;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WayneEnterprisesProductImporter_specs {
@@ -31,6 +34,28 @@ class WayneEnterprisesProductImporter_specs {
         Iterable<Product> actual = sut.fetchProducts();
 
         assertThat(actual).allMatch(x -> x.getSupplierName().equals("WAYNE"));
+    }
+
+    @ParameterizedTest
+    @DomainArgumentsSource
+    @DisplayName("공급자 상품이 가진 프로퍼티를 잘 투영한다")
+    void sut_correctly_projects_source_properties(WayneEnterprisesProduct source) {
+        var stub = new WayneEnterprisesProductSourceStub(source);
+        var sut = new WayneEnterprisesProductImporter(stub);
+
+        List<Product> products = new ArrayList<>();
+        sut.fetchProducts().forEach(products::add);
+        Product actual = products.get(0);
+
+        assertThat(actual.getProductCode()).isEqualTo(source.getId());
+        assertThat(actual.getProductName()).isEqualTo(source.getTitle());
+
+        // 우리 Product 의 가격 타입 > listPrice : 정가, discount : 할인가
+        // Wayne 이 제공하는 Product 의 가격 타입 > listPrice : 정가, sellingPrice : 판매가
+        // -> 할인가를 제공하지 않기 때문에 할인가를 계산해야 한다. (할인가 = 정가-판매가)
+        assertThat(actual.getPricing().getListPrice()).isEqualByComparingTo(Integer.toString(source.getListPrice()));
+        assertThat(actual.getPricing().getDiscount())
+                .isEqualByComparingTo(Integer.toString(source.getListPrice() - source.getSellingPrice())); // 할인가 = 정가 - 판매가
     }
 
 }
