0abfb37071df38613bbb390ea3b9935397241b05
yuhyerin <hyerin77@naver.com>
2024-03-01 18:42:22 +0900

    add ProductSynchronizer test case 3 using mock

--- src/test/java/productimporter/ProductSynchronizer_specs.java
+++ src/test/java/productimporter/ProductSynchronizer_specs.java
@@ -1,14 +1,17 @@
 package productimporter;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import productimporter.suppliers.wayneenterprises.WayneEnterprisesProduct;
 import productimporter.suppliers.wayneenterprises.WayneEnterprisesProductImporter;
 import productimporter.suppliers.wayneenterprises.WayneEnterprisesProductSourceStub;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class ProductSynchronizer_specs {
 
@@ -48,5 +51,31 @@ class ProductSynchronizer_specs {
 
         // Assert
         assertThat(spy.getLog()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("validator에 의해 검증되지 않은 상품은 저장되지 않는다")
+    void sut_does_not_save_invalid_Product_with_mock() {
+        // Arrange
+        var pricing = new Pricing(BigDecimal.TEN, BigDecimal.ONE);
+        var product = new Product("supplierName", "prodouctCode", "productName", pricing);
+
+        // mock
+        ProductImporter importer = mock(ProductImporter.class);
+        when(importer.fetchProducts())
+                .thenReturn(Arrays.asList(product));
+
+        ProductValidator validator = mock(ProductValidator.class);
+        when(validator.isValid(product)).thenReturn(false);
+
+        ProductInventory inventory = mock(ProductInventory.class);
+
+        var sut = new ProductSynchronizer(importer, validator, inventory);
+
+        // Act
+        sut.run();
+
+        // Assert
+        verify(inventory, never()).upsertProduct(product);
     }
 }
